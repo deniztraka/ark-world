b0f8af7292ce40377c036310c1677032823854ae
RELEPHANT\d.traka <d.traka@emakina.nl>
2018-10-31 16:31:22 +0300

    finally

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '/js'));
 
+app.use("/js", express.static(__dirname + '/build/js'));
+app.use("/assets", express.static(__dirname + '/build/assets'));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/build/index.html');
 });
+
 
 
 
